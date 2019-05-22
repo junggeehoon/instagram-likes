876ed6dca3cd3851cf89cf18f5cf46a063904f92
junggeehoon <junggeehoon@gmail.com>
2019-05-22 12:07:25 +0900

    id,password selector 수정

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const INSTAGRAM = 'https://www.instagram.com'; // Instagram account url
 const pressLike = async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: true
+      headless: false
     });
 
     const page = await browser.newPage();
@@ -21,20 +21,19 @@ const pressLike = async () => {
     console.log('🚀  Launching...');
 
     await page.waitFor(1000);
-    await page.click('#react-root > section > main > article > div.rgFsT > div:nth-child(2) > p > a');
+    await page.click('#react-root > section > main > article > div.rgFsT > div:nth-child(2) > p > a'); // click login
     await page.waitFor(1000);
+    await page.type('input[name=username]', config.email);
+    await page.type('input[name=password]', config.password);
+    await page.click('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button'); // click login button
 
-    await page.type('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > div > input', config.email);
-    await page.type('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(3) > div > div > input', config.password);
-
-    await page.click('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button');
     await page.waitFor(1000);
     // await page.waitForSelector('button.aOOlW.HoLwm');
     // await page.click('button.aOOlW.HoLwm');
     // await page.waitFor(1000);
     console.log("🤪  Loginned to your account!");
 
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 7; j++) {
       const postNumber = await page.evaluate(() => {
         return document.querySelectorAll(`#react-root > section > main > section > div.cGcGK > div:nth-child(1) > div > article`).length;
       })
@@ -61,5 +60,5 @@ const pressLike = async () => {
   }
 }
 
-// setInterval(visitHomepage, 3600000); // 1시간에 한번씩
+// setInterval(visitHomepage, 3600000); // 1시간에 한번씩 
 pressLike();
